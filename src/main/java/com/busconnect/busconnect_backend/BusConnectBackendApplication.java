3b8d18bb34da6ec94387d0c7584bb22ee76e1744
GabyB73 <geb.beg.73@gmail.com>
2025-10-21 21:58:38 +0200

    Initial project setup with Spring Boot and dependencies

---
+++ src/main/java/com/busconnect/busconnect_backend/BusConnectBackendApplication.java
@@ -0,0 +1,13 @@
+package com.busconnect.busconnect_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusConnectBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusConnectBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/busconnect/busconnect_backend/BusConnectBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.busconnect.busconnect_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusConnectBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
